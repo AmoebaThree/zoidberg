fd4d20cc7829b2528b54fa2379a3a54bc9d19318
Chris Alexander <chris@chris-alexander.co.uk>
2020-03-08 17:47:56 +0000

    Start and stop

--- README.md
+++ README.md
@@ -11,16 +11,36 @@ The Mastermind
 * zoidberg install
 * zoidberg update
   * Install updates to the specified config
-  * Note update and install are synonymous
+  * `update` and `install` are synonymous
 * zoidberg run file.yaml
+* zoidberg start file.yaml
   * Run the app specified in the config
   * Only ever runs what's currently installed
-  * Will fail if install hasn't been run yet
+  * Will probably silently fail if install hasn't been run yet
+  * `start` and `run` are synonymous
+* zoidberg stop file.yaml
+  * Stop the app specified in the config
 
 ## Config file
 
 The YAML file specifies:
 
-* Which nodes are available
+* Which hosts are available
 * Which services are available, and where they are from
-* Which services need to run on which node
+* Which services need to run on which host
+
+Example YAML:
+
+```
+hosts:
+    hostid1:
+        ip: 0.0.0.1
+    hostid2:
+        ip: 0.0.0.2
+services:
+    svc1:
+        source: https://git.uri/svc1.git
+        host: hostid1
+    svc2:
+        source: https://git.uri/svc2.git
+        host: hostid2
--- zoidberg.py
+++ zoidberg.py
@@ -1,6 +1,36 @@
+import subprocess
+
+def update(config):
+    pass
+
+def run(config):
+    exec_all_svcs(config, 'start')
+
+def stop(config):
+    exec_all_svcs(config, 'stop')
+
+def exec_all_svcs(config, cmd):
+    for svc, cfg in config['services'].items():
+        is_system = 'system' in cfg.keys() and cfg['system']
+
+        if not 'host' in cfg.keys() or not cfg['host'] in config['hosts'].keys():
+            print('Service ' + svc + ' missing host')
+            continue
+
+        ip = config['hosts'][cfg['host']]['user'] + '@' + config['hosts'][cfg['host']]['ip']
+
+        try:
+            print('Trying to ' + cmd + ' ' + svc + ' on ' + ip + ': ', end = '')
+            if is_system:
+                subprocess.check_output(['ssh', ip, 'sudo', 'systemctl', cmd, svc])
+            else:
+                subprocess.check_output(['ssh', ip, 'systemctl', '--user', cmd, svc])
+            print('OK')
+        except:
+            print('ERR')
+
 if __name__ == '__main__':
-    import argparse
-    import yaml
+    import argparse, yaml
 
     print('(V) (°,,,,°) (V)')
 
@@ -9,11 +39,18 @@ if __name__ == '__main__':
     parser.add_argument("config", help="Path to config YAML file")
     args = parser.parse_args()
 
-    config = yaml.safe_load(args.config)
+    print('Parsing ' + args.config)
+    config_stream = open(args.config, 'r')
+    config = yaml.safe_load(config_stream)
 
     if args.operation == 'install' or args.operation == 'update':
-        pass
-    elif args.operation == 'run':
-        pass
+        print('Installing / updating')
+        update(config)
+    elif args.operation == 'run' or args.operation == 'start':
+        print('Running')
+        run(config)
+    elif args.operation == 'stop':
+        print('Stopping')
+        stop(config)
     else:
         raise(Exception('Unknown operation'))
